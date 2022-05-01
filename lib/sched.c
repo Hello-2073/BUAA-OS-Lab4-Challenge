1a4e161a7f2e3548d804f49ac8d4185de311c512
20373358 <20373358@buaa.edu.cn>
2022-05-01 22:49:21 +0800

    lab3-2 second

--- lib/sched.c
+++ lib/sched.c
@@ -12,10 +12,10 @@
  *  3. CANNOT use `return` statement!
  */
 /*** exercise 3.15 ***/
-/*void sched_yield(void) {
-	env_run(LIST_FIRST(env_sched_list));
-}
-*/
+// void sched_yield(void) {
+// 	  env_run(LIST_FIRST(env_sched_list));
+// }
+
 void sched_yield(void) {
 	static int count = 0; // remaining time slices of current env
     static int point = 0; // current env_sched_list index
@@ -33,19 +33,21 @@ void sched_yield(void) {
      *  LIST_INSERT_TAIL, LIST_REMOVE, LIST_FIRST, LIST_EMPTY
      */
 	e = curenv;
-	if (count == 0) {
-		LIST_REMOVE(e, env_sched_link);
-		LIST_INSERT_TAIL(&env_sched_list[1 - point], e, env_sched_link);
-	
+	if (count == 0 || e == NULL) {
+		if (e != NULL) {
+			// LIST_REMOVE(e, env_sched_link);
+			LIST_INSERT_TAIL(&env_sched_list[1 - point], e, env_sched_link);
+		}
 		if (LIST_EMPTY(&env_sched_list[point]))	{
 			point = 1 - point;
 		}
 		LIST_FOREACH(e, &env_sched_list[point], env_sched_link) {
 			if (e->env_status == ENV_RUNNABLE) {
+				count = e->env_pri;
+				LIST_REMOVE(e, env_sched_link);
 				break;
 			}
-		}		
-		count = e->env_pri;
+		}
 	}
 	count--;
 	env_run(e);
